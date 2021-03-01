558cccb4571381f8f6d96efcde88c59cb5c2fa6e
sheilashix <70675711+sheilashix@users.noreply.github.com>
2021-02-28 22:39:56 -0700

    Create webAPls.js

---
+++ webAPls.js
@@ -0,0 +1,123 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex, time
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
+
+function startGame() {
+    startButton.classList.add('hide')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    time = 75
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+            time++
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        choice = prompt("what is your name ")
+        alert("You are done")
+        alert("do you wanna start again")
+        if (choice == yes) {
+            startButton.innerText = 'Restart'
+            startButton.classList.remove('hide')
+        }
+        else {
+            alert("good")
+        }
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+        time--
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+const questions = [
+    {
+        question: 'how can we use if statement?',
+        answers: [
+            { text: 'To validate a  boolen', correct: true },
+            { text: 'To chanel a condition', correct: false }
+        ]
+    },
+    {
+        question: 'what is html stands for?',
+        answers: [
+            { text: 'Web Dev Simplified', correct: false },
+            { text: 'Traversy Media', correct: false },
+            { text: 'Dev Ed', correct: false },
+            { text: ' hypertext markup language', correct: true }
+        ]
+    },
+    {
+        question: 'Is web development fun?',
+        answers: [
+            { text: 'Kinda', correct: false },
+            { text: 'YES!!!', correct: true },
+            { text: 'Um no', correct: false },
+            { text: 'IDK', correct: false }
+        ]
+    },
+    {
+        question: 'is java script same as java?',
+        answers: [
+            { text: 'yes', correct: false },
+            { text: 'no', correct: true }
+        ]
+    }
+]
